52173b04364931251000ad4ac5acba22d7523274
Cosmos <info@entercosmos.io>
2018-11-11 20:37:10 +0700

    Fix propTypes validation for allowNegativeNumbers

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ export default class NumberField extends React.Component {
         value: PropTypes.number,
         options: PropTypes.shape({
             numberFormat: PropTypes.oneOf(['decimal', 'integer']).isRequired,
-            allowNegativeNumber: PropTypes.bool.isRequired,
+            allowNegativeNumbers: PropTypes.bool.isRequired,
             precision: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8]).isRequired
         }).isRequired
     }
