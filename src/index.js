289e16706c39cb642a5095d69c315687f90084e3
keyflight <olivier@andev.nl>
2018-11-14 22:25:17 +0700

    Make precisionId optional

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export default class NumberField extends React.Component {
         options: PropTypes.shape({
             numberFormatId: PropTypes.oneOf(['decimal', 'integer']).isRequired,
             allowNegativeNumbers: PropTypes.bool.isRequired,
-            precisionId: PropTypes.oneOf(['1', '2', '3', '4', '5', '6', '7', '8']).isRequired
+            precisionId: PropTypes.oneOf(['1', '2', '3', '4', '5', '6', '7', '8'])
         }).isRequired
     }
 
