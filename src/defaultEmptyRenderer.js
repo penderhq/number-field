ab96850ade9db0a33f62ea3fb3a5e5d2fbebe328
keyflight <olivier@andev.nl>
2019-09-27 10:56:01 +0200

    Update defaultEmptyRenderer

--- src/defaultEmptyRenderer.js
+++ src/defaultEmptyRenderer.js
@@ -4,11 +4,19 @@ import {css} from 'emotion'
 export default () => (
     <div
         className={css`
-            content: '';
-            border-radius: 9999px;
-            background-color: rgba(0,0,0,0.05);
-            width: 18px;
-            height: 6px;
+            display: flex;
+            align-items: center;
+            height: 22px;
         `}
-    />
+    >
+        <div
+            className={css`
+                content: '';
+                border-radius: 9999px;
+                background-color: rgba(0,0,0,0.05);
+                width: 18px;
+                height: 6px;
+            `}
+        />
+    </div>
 )
